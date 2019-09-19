a2f5b49e4ee2820c6667c67728f1631feabb1c1c
Norin Stark <norinstark@Sousdeys-iMac.local>
2019-09-19 18:05:24 +0700

    config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = ''
-NGROK_URL = ''
+TOKEN = '869700453:AAFYPaaF_zCeIocVcdYiDNCLp4PkcpJi7t4'
+HEROKU_URL = 'https://norin-telegram-bot.herokuapp.com/'
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
-LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
+LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(HEROKU_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
 TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}'
