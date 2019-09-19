34f7f6ab0ec196ae6edf9c8bac40b4d2202bd6f1
Norin Stark <norinstark@Sousdeys-iMac.local>
2019-09-19 17:49:22 +0700

    make it better

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+import datetime
+import pymongo
+import telegram
+from telegram.ext import Updater
+from telegram import keyboardbutton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+import time
+
+from telegram_bot import TelegramBot
+from config import TELEGRAM_INIT_WEBHOOK_URL
+
+app = Flask(__name__)
+TelegramBot.init_webhook(TELEGRAM_INIT_WEBHOOK_URL)
+
+bot = telegram.Bot(token='869700453:AAFYPaaF_zCeIocVcdYiDNCLp4PkcpJi7t4')
+
+token = '869700453:AAFYPaaF_zCeIocVcdYiDNCLp4PkcpJi7t4'
+
+@app.route("/", methods=["GET"])
+def hello():
+    print('Hello World!')
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    print(data)
+
+    bot = TelegramBot()
+    bot.parse_webhook_data(data)
+    success = bot.action()
+    return jsonify(sucess=success)
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
