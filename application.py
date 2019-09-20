baf6d54c89925d0ab03d38a0e58fff76d99e60af
Norin Stark <norinstark@Sousdeys-iMac.local>
2019-09-20 11:07:29 +0700

    update

--- application.py
+++ application.py
@@ -1,33 +1,23 @@
-import os
-from flask import Flask, request
 import telebot
 
-TOKEN = '869700453:AAFYPaaF_zCeIocVcdYiDNCLp4PkcpJi7t4'
-bot = telebot.TeleBot(TOKEN)
+API_TOKEN = '869700453:AAFYPaaF_zCeIocVcdYiDNCLp4PkcpJi7t4'
 
-app = Flask(__name__)
+bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-@app.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "Ok", 200
-
-@app.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://norin-telegram-bot.herokuapp.com/' + TOKEN)
-    bot.send_message(chat_id='', text='Please come to PickUp the Package!')
-    return "Ok", 200
 
 bot.polling()
-
-if __name__ == "__main__":
-    app.run(port=int(os.environ.get('PORT', 5000)))
